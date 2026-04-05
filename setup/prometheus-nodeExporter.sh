bb67ef7ce8d185f031fcff1c0c4c6a74e2926045
Mabe <bokaj-eu@web.de>
2026-04-05 15:26:25 +0200

    Cronjob added

--- setup/prometheus-nodeExporter.sh
+++ setup/prometheus-nodeExporter.sh
@@ -4,6 +4,8 @@ cd ~
 wget https://github.com/prometheus/node_exporter/releases/download/v1.10.2/node_exporter-1.10.2.linux-amd64.tar.gz
 tar xvfz node_exporter-1.10.2.linux-amd64.tar.gz
 mv prometheus-export.sh node_exporter-1.10.2.linux-amd64/prometheus-export.sh
+rm node_exporter-1.10.2.linux-amd64.tar.gz
 cd node_exporter-1.10.2.linux-amd64
+(crontab -l 2>/dev/null; echo "*/5 * * * * ~/prometheus-export.sh") | crontab -
 ./node_exporter
 
